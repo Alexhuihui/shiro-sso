b22d5b3b98c2b41c544e7a8f29950f95e8c06d45
wangyonghui <2930807240@qq.com>
2020-01-04 10:03:34 +0800

    Initial commit

---
+++ src/main/java/top/alexmmd/ShiroSsoApplication.java
@@ -0,0 +1,13 @@
+package top.alexmmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroSsoApplication.class, args);
+	}
+
+}
